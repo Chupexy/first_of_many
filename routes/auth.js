9bfd61f065f5d0943e3f0e7d98c2a349f29bf516
chupexy <osaretin520@gmail.com>
2024-07-06 16:15:23 +0100

    added new endpoints to auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../model/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 //endpoint to signup
@@ -36,4 +37,59 @@ router.post('/signup', async(req, res) =>{
     }
 })
 
+//endoint to login
+router.post('/login', async(req, res) =>{
+    const {email, password} = req.body
+    if(!email || !password)
+        return res.status(400).send({status: 'error', msg: 'All field must be filled'})
+
+    try {
+        // get user from database
+        let user = await User.findOne({email}).lean()
+        if(!user)
+            return res.status(200).send({status: 'ok', msg:'No user with the email found'})
+
+        //compare passsword
+        const corret_password = await bcrypt.compare(password, user.password)
+        if(!corret_password)
+            return res.status(200).send({status: 'ok', msg:'Password is incorrect'})
+
+        // create token
+        const token = jwt.sign({
+            _id: user._id,
+            email: user.email
+        }, process.env.JWT_SECRET)
+
+        //update user document to online
+        user = await User.findOneAndUpdate({email}, {is_online: true}, {new: true}).lean()
+
+        //send response
+        res.status(200).send({status: 'ok', msg: 'Login Successful', user, token})
+        
+    } catch (e) {
+        console.log(error)
+        return res.status(500).send({status: 'error', msg:'An error occured'})  
+    }
+})
+
+//endpoint to logout
+router.post('/logout', async(req, res) =>{
+    const {token} = req.body
+    if(!token)
+        return res.status(400).send({status: 'error', msg: 'Token is required'})
+
+    try {
+        //verify token
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+
+        await User.updateOne({_id: user._id}, {is_online: false})
+        return res.status(200).send({status: 'ok', msg: 'Logout Successful'})
+    } catch (error) {
+        console.log(error)
+        if(error == "JsonWebTokenError")
+            return res.status(400).send({status: 'error', msg: 'Invalid token'})
+
+        return res.status(500).send({status: 'error', msg:'An error occured'})    
+    }
+})
 module.exports = router
